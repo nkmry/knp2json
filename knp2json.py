8bea1fd00759c9771922274dec54f78fa9b7a0bb
nakamura <nkmry333@gmail.com>
2014-07-11 15:45:46 +0900

    extracted the code for parsing KNP's output from QASA project

---
+++ knp2json.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+import json
+
+
+def analyze_knp(knp_tab_output):
+    # phrase = {type, relation, relationType, basics, morphemes, features}
+    # basic = {type, relation, relationType, phrase, features [, case, caseAnalysis]}
+    # morpheme = {type, phrase, morpheme, features}
+    lines = knp_tab_output.split('\n')
+    phrases = []
+    basics = []
+    morphemes = []
+    for l in lines:
+        if len(l) < 1 or l[0] in "#E":
+            continue
+        d = {}
+        f = re.split('>?<?', l)
+        if l[0] == '*':
+            d['features'] = f[1:-1]  # 最初は係り受けの情報、最後は空
+            d['relation'] = int(f[0][2:-2])
+            d['relationType'] = f[0][-2]
+            d['basics'] = []
+            d['morphemes'] = []
+            phrases.append(d)
+        elif l[0] == '+':
+            d['phrase'] = len(phrases) - 1
+            d.update(analyze_basic(f[:-1]))
+            basics.append(d)
+            phrases[-1]['basics'].append(len(basics)-1)
+        else:
+            d['phrase'] = len(phrases) - 1
+            d.update(analyze_morpheme(f[:-1]))
+            morphemes.append(d)
+            phrases[-1]['morphemes'].append(len(morphemes)-1)
+    return {"phrases": phrases, "basics": basics, "morphemes": morphemes}
+
+
+def analyze_basic(basic_info):
+    d = {'relation': int(basic_info[0][2:-2]), 'relationType': basic_info[0][-2]}
+    basic_info = basic_info[1:]
+    removed = []
+    for f in basic_info:
+        splited = f.split(':')
+        if len(splited) > 1:
+            removed.append(f)
+            if splited[0] == "解析格":
+                d['case'] = splited[1]
+            if splited[0] == "格解析結果":
+                d['caseAnalysis'] = analyze_case_analysis(splited[-1])
+    for f in removed:
+        basic_info.remove(f)
+    d['features'] = basic_info
+    return d
+
+
+def analyze_case_analysis(case_analysis_string):
+    elements = case_analysis_string.split(';')
+    d = {}
+    for element in elements:
+        splited = element.split('/')
+        e = {'flag': splited[1],
+             'expression': splited[2],
+             '#basics': changeInt(splited[3]),
+             'sentenceId': changeInt(splited[5])}
+        d[splited[0]] = e
+    return d
+
+
+def changeInt(intString):
+    if intString.isdigit():
+        return int(intString)
+    else:
+        return intString
+
+
+def analyze_morpheme(morheme_info):
+    s = morheme_info[0].split(' ')
+    d = {'input': s[0], 'pronunciation': s[1], 'original': s[2], 'pos': s[3], 'posId': s[4], 'subPos': s[5],
+         'subPosId': s[6], 'inflectionType': s[7], 'inflectionTypeId': s[8], 'inflection': s[9], 'inflectionId': s[10],
+         'others': s[11]}
+    morheme_info = morheme_info[1:]
+    removed = []
+    for f in morheme_info:
+        splited = f.split(':')
+        if len(splited) > 1:
+            removed.append(f)
+            if splited[0] == 'Wikipediaエントリ':
+                d['wikipedia'] = splited[1]
+            else:
+                d[splited[0]] = splited[1:]
+    for f in removed:
+        morheme_info.remove(f)
+    d['features'] = morheme_info
+    return d
+
+
+def show_analyzed_knp_info(analyzed_knp_info):
+    phrases = analyzed_knp_info["phrases"]
+    basics = analyzed_knp_info["basics"]
+    morphemes = analyzed_knp_info["morphemes"]
+    print("### 文節 ###")
+    attr = ['relation', 'relationType', 'basics', 'morphemes', 'features', 'type']
+    for i in range(len(phrases)):
+        print(str(i) + ': ' + convert_dictionary_to_string(phrases[i], attr))
+    print("### 形態素 ###")
+    attr = ['phrase', 'input', 'pos', 'subPos', 'wikipedia', 'features', 'type']
+    for i in range(len(morphemes)):
+        print(str(i) + ': ' + convert_dictionary_to_string(morphemes[i], attr))
+    print("### 基本句 ###")
+    attr = ['relation', 'relationType', 'phrase', 'case', 'caseAnalysis', 'features', 'type']
+    for i in range(len(basics)):
+        print(str(i) + ': ' + convert_dictionary_to_string(basics[i], attr))
+
+
+def convert_dictionary_to_string(dictionary, keys):
+    s = '{'
+    for k in keys:
+        if k in dictionary.keys():
+            s += '\'' + k + '\':' + str(dictionary[k]) + ', '
+    s += '}'
+    return s
+
+
+if __name__ == "__main__":
+    syntactic_info = analyze_knp(sys.stdin.read())
+    print(json.dumps(syntactic_info, indent=2, ensure_ascii=False))
---
+++ test/knp2json_test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+import os
+import importlib.machinery as machinery
+loader = machinery.SourceFileLoader('knp2json', os.path.dirname(os.path.abspath(__file__)) + '/../knp2json.py')
+k2j = loader.load_module()
+
+
+class Knp2jsonTest(unittest.TestCase):
+
+    def setUp(self):
+        file = open('test_knp_output.txt')
+        self.knp_info = k2j.analyze_knp(file.read())
+        file.close()
+
+    def test_analyze_knp(self):
+        self.assertEqual(len(self.knp_info["phrases"]), 14)
+        self.assertEqual(len(self.knp_info["basics"]), 20)
+        self.assertEqual(len(self.knp_info["morphemes"]), 41)
+        self.assertEqual(self.knp_info["phrases"][0]["basics"], [0, 1, 2])
+        self.assertEqual(self.knp_info["phrases"][0]["morphemes"], [0, 1, 2, 3, 4])
+        self.assertEqual(self.knp_info["phrases"][0]["relation"], 6)
+        self.assertEqual(self.knp_info["phrases"][0]["relationType"], "D")
+        self.assertEqual(self.knp_info["phrases"][-1]["basics"], [19])
+        self.assertEqual(self.knp_info["phrases"][-1]["morphemes"], [38, 39, 40])
+        self.assertEqual(self.knp_info["phrases"][-1]["relation"], -1)
+        self.assertEqual(self.knp_info["phrases"][-1]["relationType"], "D")
+        # k2j.show_analyzed_knp_info(self.knp_info)
+
+    def test_analyze_basic(self):
+        self.assertEqual(self.knp_info["basics"][0]["relation"], 1)
+        self.assertEqual(self.knp_info["basics"][0]["relationType"], "D")
+        self.assertEqual(self.knp_info["basics"][2]["case"], "ガ")
+        self.assertNotIn("解析格:ガ", self.knp_info["basics"][2]["features"])
+        self.assertEqual(self.knp_info["basics"][6]["case"], "時間")
+        self.assertNotIn("解析格:時間", self.knp_info["basics"][6]["features"])
+        self.assertEqual(self.knp_info["basics"][-1]["relation"], -1)
+        self.assertEqual(self.knp_info["basics"][-1]["relationType"], "D")
+
+    def test_analyze_case_analysis(self):
+        self.assertIn("caseAnalysis", self.knp_info["basics"][-1])
+        ca = self.knp_info["basics"][-1]["caseAnalysis"]
+        c = ca["ガ"]
+        self.assertEqual(c["flag"], "U")
+        self.assertEqual(c["expression"], "-")
+        self.assertEqual(c["#basics"], "-")
+        self.assertEqual(c["sentenceId"], "-")
+        c = ca["ヲ"]
+        self.assertEqual(c["flag"], "C")
+        self.assertEqual(c["expression"], "グッズ")
+        self.assertEqual(c["#basics"], 16)
+        self.assertEqual(c["sentenceId"], 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
